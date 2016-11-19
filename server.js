7a382d86972f66b277ac34fdc14b5c85d538da8d
Gael Arrambide <gael_arrambide@hotmail.com>
2016-11-19 11:50:47 -0600

    trying heroku

--- server.js
+++ server.js
@@ -28,5 +28,6 @@ models.sequelize.sync();
 app.use('/', routes);
 
 //Init server
-var port = 3000;
-app.listen(port);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
